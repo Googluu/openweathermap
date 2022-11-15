3783ce7b678b63984f66dac42427af060a5525f8
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-11-15 11:08:48 -0500

    03 step

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Spinner } from './Spinner';
 import city from '../assets/image/image-weather.jpeg';
+import '../assets/css/App.css';
 
 export const Card = ({ showData, loadingData, weather, forecast }) => {
 
@@ -10,8 +11,32 @@ export const Card = ({ showData, loadingData, weather, forecast }) => {
   let year = today.getFullYear();
   let date = `${day}/${month}/${year}`;
 
+  let url = "";
+  let iconUrl = "";
+
+  let iconUrl3 = "";
+  let iconUrl6 = "";
+  let iconUrl9 = "";
+
+  let forecastData3 = "";
+  let forecastData6 = "";
+  let forecastData9 = "";
+
   if (loadingData) {
     return <Spinner />
+  }
+
+  if (showData) {
+    url = "https://openweathermap.org/img/w/";
+    iconUrl = url + weather.weather[0].icon + ".png";
+
+    iconUrl3 = url + forecast.list[1].weather[0].icon + ".png";
+    iconUrl6 = url + forecast.list[1].weather[0].icon + ".png";
+    iconUrl9 = url + forecast.list[1].weather[0].icon + ".png";
+
+    forecastData3 = forecast.list[1].dt_txt.substring(8, 10) +'/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13);
+    forecastData6 = forecast.list[2].dt_txt.substring(8, 10) +'/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 13);
+    forecastData9 = forecast.list[3].dt_txt.substring(8, 10) +'/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' + forecast.list[3].dt_txt.substring(11, 13);
   }
 
   return (
@@ -23,6 +48,11 @@ export const Card = ({ showData, loadingData, weather, forecast }) => {
               <div className="col-md-4">
                 <h3 className="card-title">{weather.name}</h3>
                 <p className="card-date">{date}</p>
+                <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+                <p className="card-desc">
+                  <img src={iconUrl} alt="icon" />
+                  {weather.weather[0].description}
+                </p>
                 <img 
                   src={city} 
                   alt="city"
@@ -31,8 +61,32 @@ export const Card = ({ showData, loadingData, weather, forecast }) => {
               </div>
               <div className="col-md-8">
                 <div className="card-body text-start mt-2">
-
+                  <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}°C</h5>
+                  <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}°C</h5>
+                  <h5 className="card-text">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}°C</h5>
+                  <h5 className="card-text">Humedad: {weather.main.humidity}%</h5>
+                  <h5 className="card-text">Velocidad del viento: {weather.wind.speed}m/s</h5>
                 </div>
+                <hr/>
+
+                <div className="row mt-4">
+                  <div className="col">
+                    <p>{forecastData3}h</p>
+                    <p className="description"><img src={iconUrl3} alt="icon" />{forecast.list[1].weather[0].description}</p>
+                    <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)}°C</p>
+                  </div>
+                  <div className="col">
+                    <p>{forecastData6}h</p>
+                    <p className="description"><img src={iconUrl6} alt="icon" />{forecast.list[2].weather[0].description}</p>
+                    <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)}°C</p>
+                  </div>
+                  <div className="col">
+                    <p>{forecastData9}h</p>
+                    <p className="description"><img src={iconUrl9} alt="icon" />{forecast.list[3].weather[0].description}</p>
+                    <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)}°C</p>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
